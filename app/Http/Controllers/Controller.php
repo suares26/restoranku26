c4bf23a52e7b36e49a1f2bf48729d1c16e63aa03
Suarez <ikumrtns@gmail.com>
2026-01-29 21:38:34 +0800

    Implement Menu and Cart functionality with corresponding views; add routes for menu, cart, and checkout

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Request;
+
 abstract class Controller
 {
     //
