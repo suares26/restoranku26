a5c71c98e0a597e42eeee8f0e7885d425379e05b
Suarez <ikumrtns@gmail.com>
2026-01-29 21:36:35 +0800

    Update ItemFactory with new fields and improve data generation; modify DatabaseSeeder to include ItemSeeder

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -12,11 +12,16 @@ class ItemFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word(),
-            'price' => $this->faker->numberBetween(10000, 100000),
-            'category_id' => \App\Models\Category::factory(),
-            'img' => $this->faker->imageUrl(),
-            'is_active' => true,
+            'name' => $this->faker->name(),
+            'category_id' => $this->faker->numberBetween(1, 2),
+            'price' => $this->faker->randomFloat(2, 10000, 100000),
+            'description' => $this->faker->sentence(),
+            'img' => fake()->randomElement([
+                'https://images.unsplash.com/photo-1626497132810-f38eb29c5385',
+                'https://plus.unsplash.com/premium_photo-1664360228159-437020f924c8',
+                'https://images.unsplash.com/photo-1569718212165-3a8278d5f624',
+            ]),
+            'is_active' => $this->faker->boolean(),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Item;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            // RoleSeeder::class,
+            // CategorySeeder::class,
+            ItemSeeder::class,
+            // UserSeeder::class,
         ]);
     }
 }
